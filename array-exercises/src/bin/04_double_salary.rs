58802abaae2bb47516d8e6f6fa8e119958181922
johel <johel.carvalho@gmail.com>
2022-03-15 21:05:58 -0300

    feat: experimented map iterator with local array

--- array-exercises/src/bin/04_double_salary.rs
+++ array-exercises/src/bin/04_double_salary.rs
@@ -1,4 +1,4 @@
-use array_exercises::{User, USERS};
+use array_exercises::{Sex, User, USERS};
 
 fn main() {
     #[allow(unused)]
@@ -35,6 +35,39 @@ fn main() {
     // let lines2 = text.lines();
     // let _: Vec<&str> = lines2.map(str::trim).filter(|s| *s != "iguanas").collect();
     // println!("lines: {:?}", lines2)
+
+    let local_users = vec![
+        User {
+            id: 1,
+            city: "São Paulo",
+            name: "josé da silva",
+            age: 21,
+            weight: 70,
+            sex: Sex::M,
+            height: 1.7,
+            salary: 1000.00,
+            married: false,
+            active: true,
+        },
+        User {
+            id: 2,
+            city: "Rio de Janeiro",
+            name: "maria clementina correia",
+            age: 17,
+            weight: 70,
+            sex: Sex::F,
+            height: 1.6,
+            salary: 2000.00,
+            married: false,
+            active: true,
+        },
+    ];
+
+    println!("-----------------");
+    let items = local_double_salaries(&local_users);
+    for (index, item) in items.enumerate() {
+        println!("local doubled salary {}: {:?}", index, item);
+    }
 }
 
 fn double_salary(users: &'static [User]) -> impl Iterator<Item = f64> {
@@ -51,5 +84,9 @@ fn doubled_salaries(users: &[User]) -> Vec<f64> {
 // }
 
 fn doubles_salaries_2<'a>(users: &'a [User]) -> impl Iterator<Item = f64> + 'a {
+    users.iter().map(|item| item.salary * 2.0)
+}
+
+fn local_double_salaries(users: &[User]) -> impl Iterator<Item = f64> + '_ {
     users.iter().map(|item| item.salary * 2.0)
 }
