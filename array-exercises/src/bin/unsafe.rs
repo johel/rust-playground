96a31603745ebc96126571cce1de1057f042b3eb
johel <johel.carvalho@gmail.com>
2022-03-22 23:11:16 -0300

    feat: unsafe tickering

---
+++ array-exercises/src/bin/unsafe.rs
@@ -0,0 +1,50 @@
+use std::slice;
+
+fn main() {
+    #[allow(unused)]
+    experiment_1();
+    experiment_2();
+}
+
+fn experiment_1() {
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+        *r2 = 1;
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+        // *r1 = 2;
+        // println!("r1 is: {}", *r1);
+        // println!("r2 is: {}", *r2);
+    }
+}
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
+fn experiment_2() {
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = split_at_mut(r, 3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+}
